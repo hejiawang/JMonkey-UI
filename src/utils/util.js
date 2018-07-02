3ffc0280f58096e9f7de0efe452cb98c34ff1f15
赫家旺 <952327407@qq.com>
2018-07-02 17:14:32 +0800

    menu manager

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 /**
  * 生成随机len位数字
  */
@@ -6,4 +8,34 @@ export const randomLenNum = (len, date) => {
   random = Math.ceil(Math.random() * 100000000000000).toString().substr(0, typeof len === 'number' ? len : 4)
   if (date) random = random + Date.now()
   return random
+}
+
+/**
+ * 将树形结构转换为array
+ * @param data 树形结构对象
+ * @param expandAll 是否展开
+ * @param parent 父结构对象
+ * @param level 等级
+ */
+export const treeToArray = ( data, expandAll, parent = null, level = null ) => {
+  let treeArray = [];
+  Array.from(data).forEach(function(record) {
+
+    if (record.treeExpanded === undefined) Vue.set(record, '_expanded', expandAll); //设置节点是否展开
+
+    let _level = 1;
+    if (level !== undefined && level !== null)  _level = level + 1;
+    Vue.set(record, '_level', _level); //设置节点层级
+
+    if (parent) Vue.set(record, 'parent', parent);  //如果有父元素
+
+    treeArray.push(record);
+
+    if (record.children && record.children.length > 0) {  //递归子节点
+      const children = treeToArray(record.children, expandAll, record, _level);
+      treeArray = treeArray.concat(children)
+    }
+  });
+
+  return treeArray;
 }
--- src/views/sys/menu/menu.vue
+++ src/views/sys/menu/menu.vue
@@ -8,83 +8,88 @@
       </el-row>
       <!-- 功能按钮 end -->
 
-      <el-tree
-        :data="data5"
-        show-checkbox
-        node-key="id"
-        default-expand-all
-        :expand-on-click-node="false">
-        <span  slot-scope="{ node, data }" style="flex: 1; display: flex; align-items: center; justify-content: space-between; font-size: 14px; padding-right: 8px;">
-          <span >{{ node.label }}</span>
-          <span >{{ node.name }}</span>
-          <span> 123123123 </span>
-        </span>
-      </el-tree>
+      <el-table :data="formatMenuTreeData" ref="menuTable" height="calc(100% - 80px)" tooltip-effect="dark" :row-style="showRow">
+        <el-table-column prop="name" label="名称" show-overflow-tooltip width="200">
+          <template slot-scope="scope">
+            <span v-for="space in scope.row._level" class="ms-tree-space" :key="space"></span>
 
+            <span class="tree-ctrl" v-if="iconShow(scope.row)" @click="toggleExpanded(scope.$index)">
+              <i v-if="!scope.row._expanded" class="el-icon-plus"></i>
+              <i v-else class="el-icon-minus"></i>
+            </span>
 
+            <span>{{scope.row.name}}</span>
+          </template>
+        </el-table-column>
 
+        <el-table-column prop="type" label="类型" show-overflow-tooltip/>
+        <el-table-column prop="icon" label="图标" show-overflow-tooltip/>
+        <el-table-column prop="sort" label="排序" show-overflow-tooltip/>
+        <el-table-column prop="permission" label="权限" show-overflow-tooltip/>
+        <el-table-column prop="method" label="请求方法" show-overflow-tooltip/>
+        <el-table-column prop="url" label="请求链接" show-overflow-tooltip/>
+        <el-table-column prop="path" label="前端URL" show-overflow-tooltip/>
+        <el-table-column prop="component" label="VUE页面" show-overflow-tooltip/>
 
+      </el-table>
     </div>
   </el-container>
 </template>
 <script>
+
   import waves from "@/directive/waves/index.js"; //按钮水波纹效果
+  import { treeToArray } from "@/utils/util";
+  import '@/styles/treeTable.scss'
 
   export default {
     name: "menu",
     directives: {
       waves
     },
-    data(){
-      const data = [{
-        id: 1,
-        label: '一级 1',
-        name: 'aaaa',
-        children: [{
-          id: 4,
-          label: '二级 1-1',
-          name: '123',
-          children: [{
-            id: 9,
-            label: '三级 1-1-1',
-            name: '123',
-          }, {
-            id: 10,
-            label: '三级 1-1-2',
-            name: '123',
-          }]
-        }]
-      }, {
-        id: 2,
-        label: '一级 2',
-        name: '123',
-        children: [{
-          id: 5,
-          label: '二级 2-1',
-          name: '123',
-        }, {
-          id: 6,
-          label: '二级 2-2',
-          name: '123',
-        }]
-      }, {
-        id: 3,
-        label: '一级 3',
-        name: '123',
-        children: [{
-          id: 7,
-          label: '二级 3-1',
-          name: '123',
-        }, {
-          id: 8,
-          label: '二级 3-2',
-          name: '123',
-        }]
-      }];
+    data() {
       return {
-        data4: JSON.parse(JSON.stringify(data)),
-        data5: JSON.parse(JSON.stringify(data))
+        treeDate: [
+          { id: 0, name: '首页', type: '菜单', icon: '无', sort: '1', permission: 'home', method: 'GET', url: '/home', path: '/home', component: 'home.vue'},
+          { id: 1, name: '系统设置', type: '菜单', icon: '无', sort: '2', permission: 'sys', method: 'GET', url: '', path: '', component: '', children: [
+            { id: 2, name: '用户管理', type: '菜单', icon: '无', sort: '3', permission: 'sys_user', method: 'GET', url: '/sys/user', path: '/sys/user', component: 'user/user.vue' },
+            { id: 3, name: '角色管理', type: '菜单', icon: '无', sort: '2', permission: 'sys', method: 'GET', url: '', path: '', component: ''},
+          ]},
+        ]
       }
+    },
+    computed: {
+      formatMenuTreeData: function() {
+        let tmpTreeData = Array.isArray(this.treeDate) ? this.treeDate : [this.treeDate];
+        return treeToArray(tmpTreeData, false);
+      }
+    },
+    methods: {
+      /**
+       * 是否显示菜单树形表格展示按钮
+       * @param record
+       * @returns {boolean}
+       */
+      iconShow(record) {
+        return ( record.children && record.children.length > 0)
+      },
+      /**
+       * 控制菜单树形表格行数据展示动作
+       * @param row
+       * @returns {string}
+       */
+      showRow: function(row) {
+        const show = (row.row.parent ? (row.row.parent._expanded && row.row.parent._show) : true)
+        row.row._show = show
+        return show ? 'animation:treeTableShow 1s;-webkit-animation:treeTableShow 1s;' : 'display:none;'
+      },
+      /**
+       * 展示/隐藏 菜单树形表格行数据
+       * @param trIndex
+       */
+      toggleExpanded: function(trIndex) {
+        const record = this.formatMenuTreeData[trIndex]
+        record._expanded = !record._expanded
+      },
     }
   };
 </script>
